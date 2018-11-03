07612b8b5af33531b5cf5accfa4203f565c1aae1
YuyaoWZ <yuyaow3@illinois.edu>
2018-11-02 20:08:58 -0500

    Basic test for networking

---
+++ src/test/java/basicTest.java
@@ -0,0 +1,30 @@
+import cs425.mp3.Config;
+import cs425.mp3.FileCommand;
+import cs425.mp3.FileOperation;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedOutputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class basicTest {
+
+    @Test
+    void parseTest() throws Exception {
+        FileOperation f = new FileOperation(null);
+        Socket s = new Socket("127.0.0.1", Config.TCP_PORT);
+        ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(s.getOutputStream()));
+        out.writeObject(genFake());
+        out.flush();
+        out.close();
+        s.close();
+        System.err.println("Finished");
+        f.stopServer();
+    }
+
+    static FileCommand genFake() {
+        FileCommand fc = new FileCommand("query", "b", "asda/12", 1);
+        return fc;
+    }
+
+}
