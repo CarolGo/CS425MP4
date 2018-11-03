058714c790366b0fc732a3fdbdf30fed7e6ca602
YuyaoWZ <yuyaow3@illinois.edu>
2018-11-03 12:33:05 -0400

    Tech demo on send/recv file

--- src/test/java/sendRecvTest.java
+++ src/test/java/sendRecvTest.java
@@ -11,38 +11,59 @@ public class sendRecvTest {
         new Thread(() -> {
             try {
                 Socket socket = sc.accept();
-                saveFileViaSocket("tttt", socket);
+                saveFileViaSocket(socket);
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
         Socket s = new Socket("127.0.0.1", Config.TCP_FILE_TRANS_PORT);
-        sendFileViaSocket("a", s);
+        sendFileViaSocket("a", s, "SDFSA");
         s.close();
     }
 
-    private static void sendFileViaSocket(String originalFilePath, Socket socket) throws IOException {
-        socket.setSoTimeout(120_000); // 120s timeout
-        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(originalFilePath))) {
-            bufferedReadWrite(in, socket.getOutputStream(), Config.NETWORK_BUFFER_SIZE);
+    private static boolean sendFileViaSocket(String originalFilePath, Socket socket, String sdfsName) {
+        try {
+            socket.setSoTimeout(120_000); // 120s timeout
+            File toSend = new File(originalFilePath);
+            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(toSend))) {
+                DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+                dOut.writeUTF(sdfsName);
+                dOut.writeLong(toSend.length());
+                bufferedReadWrite(in, dOut, Config.NETWORK_BUFFER_SIZE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
         System.err.println("Sent");
+        return true;
     }
 
     /**
      * Receive a file via socket, do nothing with socket
      *
-     * @param newFileName File name (UUID) of the file
-     * @param socket      A socket produced by ServerSocket.accept()
+     * @param socket A socket produced by ServerSocket.accept()
      */
-    private static void saveFileViaSocket(String newFileName, Socket socket) throws IOException {
-        File dest = new File(Config.STORAGE_PATH, newFileName);
-        socket.setSoTimeout(120_000); // 120s timeout
-        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(dest))) {
-            bufferedReadWrite(socket.getInputStream(), bos, Config.NETWORK_BUFFER_SIZE);
+    private static boolean saveFileViaSocket(Socket socket) {
+        try {
+            socket.setSoTimeout(120_000); // 120s timeout
+            DataInputStream dIn = new DataInputStream(socket.getInputStream());
+            String sdfsName = dIn.readUTF();
+            long fileSize = dIn.readLong();
+            System.err.println(sdfsName);
+            System.err.println(fileSize);
+            //TODO: Actual sdfsName should be a UUID
+            File dest = new File(Config.STORAGE_PATH, sdfsName);
+            try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(dest))) {
+                bufferedReadWrite(dIn, bos, Config.NETWORK_BUFFER_SIZE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
         System.err.println("Recv");
+        return true;
     }
 
     private static void bufferedReadWrite(InputStream in, OutputStream out, int bSize) throws IOException {
