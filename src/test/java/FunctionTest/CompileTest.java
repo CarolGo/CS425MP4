3648e7bfcac30e7e98fdb030d9e414e98673c4a6
YuyaoWZ <yuyaow3@illinois.edu>
2018-11-26 12:45:16 -0600

    Working loader?

--- src/test/java/FunctionTest/CompileTest.java
+++ src/test/java/FunctionTest/CompileTest.java
@@ -1,41 +1,36 @@
 package FunctionTest;
 
+import cs425.crane.function.FunctionLoader;
 import cs425.crane.function.Mp4Function;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CompileTest {
 
-    private static Mp4Function parseClass(File classFilePath, String classPath) throws IOException {
-        try (URLClassLoader cl = URLClassLoader.newInstance(new URL[]{classFilePath.toURI().toURL()})) {
-            Class<?> rawInput = Class.forName(classPath, true, cl);
-            Class<? extends Mp4Function> newClass = rawInput.asSubclass(Mp4Function.class);
-            Constructor<? extends Mp4Function> constructor = newClass.getConstructor();
-            return constructor.newInstance();
-        } catch (ReflectiveOperationException e) {
-            // Class not found or can not new etc.
-            return null;
-        }
-    }
+    @Test
+    void loaderTest() throws Exception {
+        File folder = new File("target\\test-classes", "FunctionTest");
+        Constructor<? extends Mp4Function> constructor =
+                FunctionLoader.loadClass(folder.getAbsolutePath(), "FunctionTest.UniversalFunction");
+        assertNotNull(constructor);
+        Mp4Function m = constructor.newInstance();
+        assertNotNull(m);
 
-    public static void main(String... args) throws Exception {
-        File folder = new File("target\\test-classes", "FunctionTest.UniversalFunction.class");
-
-        Mp4Function m = parseClass(folder, "FunctionTest.UniversalFunction");
-        if (m == null) {
-            System.err.println("Class bad defined");
-            return;
-        }
         //TODO: Change <Integer, List<Integer>> to a wrapper object
         Function<Integer, List<Integer>> s = m.mp4Task();
-        List<Integer> rndList = s.apply(1000);
-        System.out.println(String.format("Total lenth: %d\n11th element: %d", rndList.size(), rndList.get(11)));
+
+        int r = new Random().nextInt(5000);
+        List<Integer> rndList = s.apply(r);
+
+        assertEquals(r, rndList.size());
     }
 
 }
