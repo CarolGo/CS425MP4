1c78d96b1a8381cd1568a8b7d96cc4246a7595c5
YuyaoWZ <yuyaow3@illinois.edu>
2018-11-25 18:55:25 -0600

    test1

--- src/test/java/FunctionTest/BasicFuncTest.java
+++ src/test/java/FunctionTest/BasicFuncTest.java
@@ -3,6 +3,10 @@ package FunctionTest;
 import cs425.crane.function.SeFunction;
 
 import java.io.*;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -10,7 +14,6 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class BasicFuncTest {
-    private static final Random r = new Random();
     private static final int numCount = 100_000;
 
     private static void timing(SeFunction<Void, Void> r) {
@@ -25,6 +28,7 @@ public class BasicFuncTest {
 
     private static SeFunction<Void, Void> genArray() {
         return vv -> {
+            Random r = new Random();
             ArrayList<Integer> a = new ArrayList<>();
             for (int i = 0; i < numCount; i++) {
                 a.add(r.nextInt());
@@ -36,6 +40,7 @@ public class BasicFuncTest {
 
     private static SeFunction<Void, Void> genLinked() {
         return vv -> {
+            Random r = new Random();
             LinkedList<Integer> a = new LinkedList<>();
             for (int i = 0; i < numCount; i++) {
                 a.add(r.nextInt());
@@ -43,6 +48,20 @@ public class BasicFuncTest {
             int t = a.getLast();
             return null;
         };
+    }
+
+    // Ref: https://gist.github.com/eshrubs/29df2d808c15ed5333e709e9e34eafd7
+    private static SerializedLambda getSerialized(SeFunction s) throws Exception {
+        // There is a private method made by the compiler called ${writeReplace}
+        // that converts this lambda to a SerializedLambda.
+        // Expose that method and run it!
+        Method writeReplace = AccessController.doPrivileged((PrivilegedExceptionAction<Method>) () -> {
+            Method method = s.getClass().getDeclaredMethod("writeReplace");
+            method.setAccessible(true);
+            return method;
+        });
+        SerializedLambda res = (SerializedLambda) writeReplace.invoke(s);
+        return res;
     }
 
     private static SeFunction<Void, Void> readFuncFromFile(String fp) throws Exception {
@@ -55,10 +74,10 @@ public class BasicFuncTest {
 
     public static void main(String... args) throws Exception {
         try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("f1_arr.f")))) {
-            oos.writeObject(genArray());
+            oos.writeObject(getSerialized(genArray()));
         }
         try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("f1_lk.f")))) {
-            oos.writeObject(genLinked());
+            oos.writeObject(getSerialized(genLinked()));
         }
 
         timing(readFuncFromFile("f1_arr.f"));
